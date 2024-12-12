2d0bbd8b50d9c851de7ba512be04a304fea93237
Fadhil Fasalu Rahiman <ffasalurahiman@hof-university.de>
2024-12-12 07:21:39 +0100

    Game; start Game in tempGame

--- src/Game.java
+++ src/Game.java
@@ -1,23 +1,26 @@
 public class Game {
-    private Airplane airplane;
-    private Pilot pilot;
-    private CoPilot copilot;
-    private int roundNumber;
+    private final Airplane airplane;
+    private final Pilot pilot;
+    private final CoPilot copilot;
+    //private int roundNumber;
     private int rerollsAvailable;
-    private int startingPlayer;
-    private ApproachTrack approachTrack;
+    //private int startingPlayer;
+    private final ApproachTrack approachTrack;
+    private final AltitudeTrack altitudeTrack;
     private Players currentPlayer;
-    private final int maxRounds = 5;
-    private AltitudeTrack altitudeTrack;
+    //private final int maxRounds = 5;
 
     public Game() {
         this.airplane = new Airplane();
-        this.pilot = new Pilot();
-        this.copilot = new CoPilot();
-        this.roundNumber = 1;
+        this.pilot = new Pilot(this);
+        this.copilot = new CoPilot(this);
+        //this.roundNumber = 1;
         this.rerollsAvailable = 0;
         this.currentPlayer = pilot;
         this.approachTrack = new ApproachTrack();
+        this.altitudeTrack = new AltitudeTrack(this);
+
+        airplane.setGame(this);
     }
 
     public Pilot getPilot() {
@@ -28,6 +31,66 @@ public class Game {
     }
     public Airplane getAirplane() {
         return airplane;
+    }
+
+    public Players getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public void switchPlayer() {
+        currentPlayer = (currentPlayer == pilot) ? copilot : pilot;
+        System.out.println("Next turn.");
+    }
+
+    public ApproachTrack getApproachTrack() {
+        return approachTrack;
+    }
+    public AltitudeTrack getAltitudeTrack() {
+        return altitudeTrack;
+    }
+
+    public void setRerollsAvailable() {
+        this.rerollsAvailable++;
+    }
+    public void decreaseRerollsAvailable() {
+        if (rerollsAvailable > 0) {
+            rerollsAvailable--;
+        }
+    }
+    public int getRerollsAvailable() {
+        return rerollsAvailable;
+    }
+
+    public boolean checkCrash() {
+        if (airplane.getAltitude() < 0) {
+            System.out.println("Crash: Altitude below safe levels");
+            return true;
+        }
+        /*int currentPosition = airplane.getApproachPosition();
+        if (approachTrack.hasAirplanesAt(currentPosition)) {
+            System.out.println("Crash: Collision detected at position " + currentPosition + ".");
+            return true;
+        }*/
+        return false;
+    }
+
+    public boolean checkCrashMove(int moves) {
+        if(approachTrack.hasAirplanesAt(airplane.getApproachPosition())){
+            return true;
+        }
+        if(moves == 2){
+            if(approachTrack.hasAirplanesAt(airplane.getApproachPosition()+1)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkCrashAxis(){
+        if (airplane.getAxis().getAxisValue() < -2 || airplane.getAxis().getAxisValue() > 2) {
+            System.out.println("Crash: Axis out of balance");
+            return true;
+        }
+        return false;
     }
 
     public boolean checkWin() {
@@ -41,31 +104,6 @@ public class Game {
         }
         return false;
     }
-
-    public boolean checkCrash() {
-        if (airplane.getAxis().getAxisValue() < -2 || airplane.getAxis().getAxisValue() > 2) {
-            System.out.println("Crash: Axis out of balance");
-            return true;
-        }
-        if (airplane.getAltitude() < 0) {
-            System.out.println("Crash: Altitude below safe levels");
-            return true;
-        }
-        int currentPosition = airplane.getApproachPosition();
-        if (approachTrack.hasAirplanesAt(currentPosition)) {
-            System.out.println("Crash: Collision detected at position " + currentPosition + ".");
-            return true;
-        }
-        return false;
-    }
-    public void switchPlayer() {
-        currentPlayer = (currentPlayer == pilot) ? copilot : pilot;
-        System.out.println("Next turn: " + currentPlayer.getClass().getSimpleName());
-    }
-    public Players getCurrentPlayer() {
-        return currentPlayer;
-    }
-
 
     public boolean checkLandingConditions() {
         if (airplane.getLandingGear().getActivatedLandingGearFields() < 3) {
@@ -85,6 +123,13 @@ public class Game {
             return false;
         }
         return true;
+    }
+
+    public void checkRoundConditions(){
+        if(!pilot.isAxis() || !copilot.isAxis() || !pilot.isThrottle() || !copilot.isThrottle()){
+            System.out.println("Round conditions not met. Plane Crashed.");
+            System.exit(0);
+        }
     }
 
 
